f62bdae9654ec045f5970f6e5390d3c6fc03a99c
Nadie-the-4th <sillygoosebehavior@gmail.com>
2025-01-25 12:37:48 -0500

    Dialogue+ Event Stuff

---
+++ Assets/Scripts/Nadie/(template)yarnscript.cs
@@ -0,0 +1,16 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Yarn.Unity;
+
+// public class YarnInteract : MonoBehaviour
+// {
+//     public String StartNode;
+//     [SerializeField] DialogueRunner Dialogue;
+
+//     public void Interact()
+//     {
+//         Dialogue.StartDialogue(StartNode);
+//     }
+// }
---
+++ Assets/Scripts/Nadie/coroutine.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn;
+using Yarn.Unity;
+
+public class coroutine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TimedDialogue());
+    }
+
+    public string StartNode;
+    [SerializeField] DialogueRunner Dialogue;
+
+    // public void Interact()
+    // {
+    //     Dialogue.StartDialogue(StartNode);
+
+
+    // }
+
+    private IEnumerator TimedDialogue()
+    {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("Hi");
+        Dialogue.StartDialogue(StartNode);
+
+        yield return new WaitForSeconds(145f);
+        Debug.Log("Hey");
+    }
+}
