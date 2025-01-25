320a7c2c5eb863e3c33ed1eb46c6ed6945f30296
Sondra Boyle <sb359120@ohio.edu>
2025-01-25 16:42:41 -0500

    movement finalized
    locator points to direction, bubble doesn't move

--- Assets/Scripts/Walker/playerController.cs
+++ Assets/Scripts/Walker/playerController.cs
@@ -23,6 +23,8 @@ public class playerController : MonoBehaviour
         float rotate = Input.GetAxis("Vertical"); //y and b
         playerRb.AddTorque(rotationForce * rotate);
 
+
+
         if (Input.GetKeyDown("space"))
         {
             playerRb.AddForce(transform.right * moveForce);
