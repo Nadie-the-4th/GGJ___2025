553fd578aed4ac1984e3d17b2227fa7547f30484
Sondra Boyle <sb359120@ohio.edu>
2025-01-24 21:08:10 -0500

    added timer script and scripts folder

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    //references Scenes
+    int currentSceneIndex;
+
+    //Timer Functionality
+    float currentTime;
+    float startTime = 300f;
+    public bool timesUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
