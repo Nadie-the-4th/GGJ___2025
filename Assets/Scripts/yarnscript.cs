2d6fad5aa8edc53df2f6cc96f32d6fe28a33428e
Nadie-the-4th <sillygoosebehavior@gmail.com>
2025-01-24 21:30:31 -0500

    Yarn stuff

---
+++ Assets/Scripts/yarnscript.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnInteract : MonoBehaviour
+{
+    public String StartNode;
+    [SerializeField] DialogueRunner Dialogue;
+
+    public void Interact()
+    {
+        Dialogue.StartDialogue(StartNode);
+
+
+    }
+}
