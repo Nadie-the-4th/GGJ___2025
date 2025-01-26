39f39a514818f7b0a9f81782672e5d519f91bbbd
Sondra Boyle <sb359120@ohio.edu>
2025-01-26 03:47:09 -0500

    Background color on blend tree
    Blends between colors on a spectrum based on a float. Set to arrow keys for now

--- Assets/Scripts/Walker/ChangeBGColor.cs
+++ Assets/Scripts/Walker/ChangeBGColor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ChangeBGColor : MonoBehaviour
 {
+    public float yInput;
+    public float xInput;
     Animator BGColors;
     // Start is called before the first frame update
     void Start()
@@ -14,19 +16,26 @@ public class ChangeBGColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("p"))
-        {
-            BGColors.SetTrigger("Pink");
-        }
+        //if (Input.GetKeyDown("p"))
+        //{
+        //    BGColors.SetTrigger("Pink");
+        //}
 
-        if (Input.GetKeyDown("b"))
-        {
-            BGColors.SetTrigger("Blue");
-        }
+        //if (Input.GetKeyDown("b"))
+        //{
+        //    BGColors.SetTrigger("Blue");
+        //}
 
-        if (Input.GetKeyDown("u"))
-        {
-            BGColors.SetTrigger("Purple");
-        }
+        //if (Input.GetKeyDown("u"))
+        //{
+        //    BGColors.SetTrigger("Purple");
+        //}
+
+        float yInput = Input.GetAxis("Vertical"); //y and b
+        float xInput = Input.GetAxis("Horizontal");
+
+        BGColors.SetFloat("yInput", Mathf.Abs(yInput));
+        BGColors.SetFloat("xInput", Mathf.Abs(xInput));
+
     }
 }
